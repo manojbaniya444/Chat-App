84e2dfd6e73d642444eca45fbf6acad2d3bf675a
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2023-12-05 19:23:13 +0545

    Typing animation style modify

--- Frontend/src/ui/components/TypingAnimation.jsx
+++ Frontend/src/ui/components/TypingAnimation.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const TypingAnimation = () => {
   return (
     <div className="flex items-center pl-2">
-      <div className="w-3 h-3 bg-blue-500 rounded-full mr-1 animate-bounce"></div>
-      <div className="w-3 h-3 bg-blue-500 rounded-full mr-1 animate-bounce"></div>
-      <div className="w-3 h-3 bg-blue-500 rounded-full animate-bounce"></div>
+      <div className="w-2 h-2 bg-blue-500 rounded-full mr-1 animate-bounce"></div>
+      <div className="w-2 h-2 bg-blue-500 rounded-full mr-1 animate-bounce"></div>
+      <div className="w-2 h-2 bg-blue-500 rounded-full animate-bounce"></div>
     </div>
   );
 };
